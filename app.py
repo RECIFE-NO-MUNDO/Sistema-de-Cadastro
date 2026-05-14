a95c60a3821800e1c115b5fc832421a0d0ceed2c
RECIFE-NO-MUNDO <clailtonrodrigues@hotmail.com>
2026-05-13 22:12:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -302,7 +302,17 @@ def pdf_aluno(id):
     c.save()
 
     return send_file(arquivo, as_attachment=True)
+    
+   @app.route("/excluir_todos")
+    def excluir_todos():
+      conn = conectar()
 
+      conn.execute("DELETE FROM alunos")
+
+      conn.commit()
+      conn.close()
+
+    return redirect("/")
 
 if __name__ == "__main__":
     criar_banco()
